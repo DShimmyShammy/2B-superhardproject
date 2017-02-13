e4ddeb7e9b83bd69d8d124ac78f677f9756b38e0
PrithviPatel <mac6@129mac18.valleychristian.net>
2017-02-13 10:37:41 -0800

    Random color generator

--- webapp.js
+++ webapp.js
@@ -1,2 +1,18 @@
 // JavaScript Document
-  
+
+// random number from 0-255
+function getRndInteger() {
+	return Math.floor(Math.random() * 256);
+}
+
+function getRndColor() {
+	var r = getRndInteger(); // 0 = 00
+	var g = getRndInteger(); // 255 = FF
+	var b = getRndInteger(); // 30 = 1f
+	
+	r = r.toString(16);
+	g = g.toString(16);
+	b = b.toString(16);
+	
+	return "#" + r + g + b;
+}
